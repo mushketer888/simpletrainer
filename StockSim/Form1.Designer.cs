9eb654521a4fcf0a9f1a4c03557379939cffe1aa
Ilja <mushketer888@gmail.com>
2018-12-09 20:20:48 +0200

    init

---
+++ StockSim/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace StockSim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buybtn = new System.Windows.Forms.Button();
+            this.shortbtn = new System.Windows.Forms.Button();
+            this.closebtn = new System.Windows.Forms.Button();
+            this.poslabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 2);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(893, 515);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next bar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Balance:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total profit:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(495, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Balance:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // buybtn
+            // 
+            this.buybtn.Location = new System.Drawing.Point(108, 31);
+            this.buybtn.Name = "buybtn";
+            this.buybtn.Size = new System.Drawing.Size(75, 23);
+            this.buybtn.TabIndex = 6;
+            this.buybtn.Text = "BUY";
+            this.buybtn.UseVisualStyleBackColor = true;
+            this.buybtn.Click += new System.EventHandler(this.buybtn_Click);
+            // 
+            // shortbtn
+            // 
+            this.shortbtn.Location = new System.Drawing.Point(189, 31);
+            this.shortbtn.Name = "shortbtn";
+            this.shortbtn.Size = new System.Drawing.Size(75, 23);
+            this.shortbtn.TabIndex = 7;
+            this.shortbtn.Text = "SHORT";
+            this.shortbtn.UseVisualStyleBackColor = true;
+            this.shortbtn.Click += new System.EventHandler(this.shortbtn_Click);
+            // 
+            // closebtn
+            // 
+            this.closebtn.Enabled = false;
+            this.closebtn.Location = new System.Drawing.Point(270, 31);
+            this.closebtn.Name = "closebtn";
+            this.closebtn.Size = new System.Drawing.Size(75, 23);
+            this.closebtn.TabIndex = 8;
+            this.closebtn.Text = "CLOSE";
+            this.closebtn.UseVisualStyleBackColor = true;
+            this.closebtn.Click += new System.EventHandler(this.closebtn_Click);
+            // 
+            // poslabel
+            // 
+            this.poslabel.AutoSize = true;
+            this.poslabel.Location = new System.Drawing.Point(28, 71);
+            this.poslabel.Name = "poslabel";
+            this.poslabel.Size = new System.Drawing.Size(46, 13);
+            this.poslabel.TabIndex = 9;
+            this.poslabel.Text = "poslabel";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(296, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "50";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(268, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Lot";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(330, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(429, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Order profit:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(495, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Balance:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.buybtn);
+            this.groupBox1.Controls.Add(this.poslabel);
+            this.groupBox1.Controls.Add(this.shortbtn);
+            this.groupBox1.Controls.Add(this.closebtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 523);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(829, 106);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stats";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 627);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "SimpleTrainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buybtn;
+        private System.Windows.Forms.Button shortbtn;
+        private System.Windows.Forms.Button closebtn;
+        private System.Windows.Forms.Label poslabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ StockSim/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Linq;
+namespace StockSim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<dbdata> vals=new List<dbdata>();
+        List<dbdata> k = new List<dbdata>();
+
+        int ind = 0;
+        int maxbars = 150;
+
+        double balance=100000;
+        double pat = 0;
+        int psize;
+        int type = 0;//0 no 1 buy 2 sell
+
+        public void BlockBtn()
+        {
+            if (type == 0)
+            {
+                buybtn.Enabled = true;
+                shortbtn.Enabled = true;
+                closebtn.Enabled = false;
+                return;
+            }
+           
+                buybtn.Enabled = false;
+                shortbtn.Enabled = false;
+                closebtn.Enabled = true;
+                return;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+            UpdStats();
+
+
+
+
+
+            vals = File.ReadAllLines("eurusd.csv").Skip(1)
+                                           .Select(v => dbdata.FromCsv(v))
+                                           
+                                           .ToList();
+           
+            k = vals.Skip(ind).Take(maxbars).ToList();
+            LoadChart(k);
+            
+           
+        }
+        public void LoadChart(List<dbdata> k)
+        {
+            Series ser = new Series();
+            Series price = new Series("price"); // <<== make sure to name the series "price"
+            price.Color = Color.Black;
+            if (chart1.Series.Count==0) 
+                chart1.Series.Add(price);
+            chart1.ChartAreas[0].BorderColor = Color.Gray;
+
+            chart1.ChartAreas[0].AxisX.LabelAutoFitStyle = LabelAutoFitStyles.WordWrap;
+            chart1.ChartAreas[0].AxisX.IsLabelAutoFit = true;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = true;
+            // Set series chart type
+            chart1.Series["price"].ChartType = SeriesChartType.Candlestick;
+
+            // Set the style of the open-close marks
+            chart1.Series["price"]["OpenCloseStyle"] = "Triangle";
+
+            // Show both open and close marks
+            chart1.Series["price"]["ShowOpenClose"] = "Both";
+
+            // Set point width
+            chart1.Series["price"]["PointWidth"] = "1.0";
+
+            // Set colors bars
+            chart1.Series["price"]["PriceUpColor"] = "Green"; // <<== use text indexer for series
+            chart1.Series["price"]["PriceDownColor"] = "Red"; // <<== use text indexer for series
+
+            chart1.Series["price"].Points.Clear();
+            for (int i = 0; i < k.Count; i++)
+            {
+                // adding date and high
+                chart1.Series["price"].Points.AddXY(DateTime.Parse(k[i].Date), k[i].High);
+                // adding low
+                chart1.Series["price"].Points[i].YValues[1] = k[i].Low;
+                //adding open
+                chart1.Series["price"].Points[i].YValues[2] = k[i].Open;
+                // adding close
+                chart1.Series["price"].Points[i].YValues[3] = k[i].Close;
+            }
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Console.WriteLine(1);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ind++;
+            
+            k = vals.Skip(ind).Take(maxbars).ToList();
+            LoadChart(k);
+            UpdStats();
+        }
+
+        private void buybtn_Click(object sender, EventArgs e)
+        {
+            type = 1;//buy
+            int mane;
+            int.TryParse(textBox1.Text, out mane);
+            mane = (int)(mane * 0.01 * balance);
+            System.Console.WriteLine(String.Format("Buy power: {0}", mane));
+
+            psize = (int)(mane / k.Last().Close);
+            System.Console.WriteLine(String.Format("Lot size: {0}", psize));
+
+            balance = balance - (psize * k.Last().Close);
+            pat = k.Last().Close;
+
+            BlockBtn();
+            UpdStats();
+        }
+        public double CalcProfit()
+        {
+            double prof=0;
+
+            if (type == 1)
+            {
+                prof = (k.Last().Close / (pat) - 1)*100;//1 2
+            }
+
+            if (type == 2)
+            {
+                prof = ((pat /k.Last().Close ) - 1) * 100;//1 2
+            }
+
+            return prof;
+        }
+        private void UpdStats()
+        {
+            label2.Text = String.Format("{0:0}",balance);
+            label4.Text = String.Format("{0:+0.##;-0.##}%", ((balance / 100000) - 1) * 100);
+            if (type == 0)
+            {
+                poslabel.Text = "No Orders";
+                label8.Text = "N/A";
+            }
+            else
+            {
+                poslabel.Text = String.Format("{0} {1} @ {2}", (type == 1 ? "BUY" : "SHORT"), psize, pat);
+            }
+
+            if (type != 0)
+            {
+                label8.Text = String.Format("{0:+0.##;-0.##}%", CalcProfit());
+            }
+        }
+
+        private void closebtn_Click(object sender, EventArgs e)
+        {
+
+            if (type == 1)
+            {
+                balance = balance + (psize * k.Last().Close);
+            }
+
+            if (type == 2)
+            {
+                System.Console.WriteLine("pat {0}", pat);
+                System.Console.WriteLine("Last {0}", k.Last().Close);
+                System.Console.WriteLine("Psize {0}", psize);
+                balance = balance + (psize *pat* (pat/k.Last().Close));
+            }
+        //            pat 1.45029
+        //Last 1.41776
+        //Psize 34475
+            type = 0;
+            BlockBtn();
+            UpdStats();
+        }
+
+        private void shortbtn_Click(object sender, EventArgs e)
+        {
+            type = 2;//short
+            int mane;
+            int.TryParse(textBox1.Text, out mane);
+            mane = (int)(mane * 0.01 * balance);
+            System.Console.WriteLine(String.Format("Buy power: {0}", mane));
+
+            psize = (int)(mane / k.Last().Close);
+            System.Console.WriteLine(String.Format("Lot size: {0}", psize));
+
+            balance = balance - (psize * k.Last().Close);
+            pat = k.Last().Close;
+
+            BlockBtn();
+            UpdStats();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class dbdata
+    {
+        public string Date;
+        public double High;
+        public double Low;
+        public double Open;
+        public double Close;
+        public dbdata(string d, float h, float l, float o, float c) { Date = d; High = h; Low = l; Open = o; Close = c; }
+        public dbdata() { }
+        public void Debug()
+        {
+            System.Console.WriteLine(String.Format("{0},{1},{2},{3},{4}",this.Date,this.Open,this.High,this.Low,this.Close));
+        }
+        public static dbdata FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            dbdata dailyValues = new dbdata();
+            dailyValues.Date = values[0];
+            dailyValues.Open = Convert.ToDouble(values[1]);
+            dailyValues.High = Convert.ToDouble(values[2]);
+            dailyValues.Low = Convert.ToDouble(values[3]);
+            dailyValues.Close = Convert.ToDouble(values[4]);
+           
+            return dailyValues;
+        }
+    }
+}
